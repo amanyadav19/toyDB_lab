ece29244f587ec6aee2312da174f9e04e7f2dc37
Aman Yadav <amanyadav@cse.iitb.ac.in>
2022-02-15 01:25:33 +0530

    Task 2 done

---
+++ dblayer/loaddb.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <ctype.h>
+#include "codec.h"
+#include "../pflayer/pf.h"
+#include "../amlayer/am.h"
+#include "tbl.h"
+#include "util.h"
+
+#define checkerr(err) {if (err < 0) {PF_PrintError(); exit(1);}}
+
+#define MAX_PAGE_SIZE 4000
+
+
+#define DB_NAME "data.db"
+#define INDEX_NAME "data.db.0"
+#define CSV_NAME "data.csv"
+
+
+/*
+Takes a schema, and an array of strings (fields), and uses the functionality
+in codec.c to convert strings into compact binary representations
+ */
+int
+encode(Schema *sch, char **fields, byte *record, int spaceLeft) {
+    int n = sch->numColumns;
+    int totalSpace = spaceLeft;
+    for (int i = 0; i < n; i++) {
+        if(sch->columns[i]->type == VARCHAR) {
+            int res = EncodeCString(fields[i], record + (totalSpace - spaceLeft), spaceLeft);
+            spaceLeft = spaceLeft - res;
+        } else if(sch->columns[i]->type == INT) {
+            int res = EncodeInt(atoi(fields[i]), record + (totalSpace - spaceLeft));
+            spaceLeft = spaceLeft - res;
+        } else if(sch->columns[i]->type == LONG) {
+            int res = EncodeLong(atol(fields[i]), record + (totalSpace - spaceLeft));
+            spaceLeft = spaceLeft - res;
+        }
+    }
+    return totalSpace - spaceLeft;
+    // for each field
+    //    switch corresponding schema type is
+    //        VARCHAR : EncodeCString
+    //        INT : EncodeInt
+    //        LONG: EncodeLong
+    // return the total number of bytes encoded into record
+}
+
+Schema *
+loadCSV() {
+    // Open csv file, parse schema
+    FILE *fp = fopen(CSV_NAME, "r");
+    if (!fp) {
+	perror("data.csv could not be opened");
+        exit(EXIT_FAILURE);
+    }
+
+    char buf[MAX_LINE_LEN];
+    char *line = fgets(buf, MAX_LINE_LEN, fp);
+    if (line == NULL) {
+	fprintf(stderr, "Unable to read data.csv\n");
+	exit(EXIT_FAILURE);
+    }
+    // Open main db file
+    Schema *sch = parseSchema(line);
+    Table *tbl;
+    if(Table_open(DB_NAME, sch, true, &tbl) < 0) {
+        return NULL;
+    }
+
+    char *tokens[MAX_TOKENS];
+    char record[MAX_PAGE_SIZE];
+
+    int err;
+    if(sch->columns[2]->type == INT) {
+        err = AM_CreateIndex(DB_NAME, 0, 'i', 4);
+    } else {
+        err = -1;
+    }
+    checkerr(err);
+    int indexFD = PF_OpenFile(INDEX_NAME);
+	checkerr(indexFD);
+
+    while ((line = fgets(buf, MAX_LINE_LEN, fp)) != NULL) {
+	int n = split(line, ",", tokens);
+	assert (n == sch->numColumns);
+	int len = encode(sch, tokens, record, sizeof(record));
+	RecId rid;
+
+	rid = Table_Insert(tbl, record, len, &rid);
+
+	printf("%d %s\n", rid, tokens[0]);
+
+	// Indexing on the population column 
+	int population = atoi(tokens[2]);
+
+	
+	// Use the population field as the field to index on
+    err = AM_InsertEntry(indexFD, 'i', 4, population, rid);
+    
+    }
+    fclose(fp);
+    Table_Close(tbl);
+    err = PF_CloseFile(indexFD);
+    checkerr(err);
+    return sch;
+}
+
+int
+main() {
+    loadCSV();
+}
